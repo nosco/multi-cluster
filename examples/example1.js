1bf9dffc10005ef7faf32def8f20c5a050ce66f7
Dan Larsen <dan@techba.se>
2013-04-01 13:23:18 +0200

    The watcher now works per app, instead of reloading the entire app
    Also changed tabs == 4 spaces to tab == 2 spaces

--- examples/example1.js
+++ examples/example1.js
@@ -6,7 +6,6 @@ var multiCluster1 = new MultiCluster('worker_example1.js');
 var multiCluster2 = new MultiCluster('worker_example2.js');
 
 setInterval(function() {
-        MultiCluster.aggregateStats()
-            .then(console.log);
-    }
-    , 2000);
+  MultiCluster.aggregateStats()
+    .then(console.log);
+}, 2000);
